b39c882499aa797544860ba4a709e102e232e8d3
isaahaqq-star <isaahaqq@gmail.com>
2026-09-02 03:25:44 -0400

    data: Add 401K track list and album metadata

---
+++ player/lib/tracks.ts
@@ -0,0 +1,82 @@
+export const TRACKS_401K = [
+  {
+    id: 1,
+    title: "STOP PLAYING WITH ME",
+    duration: 0,
+    track_number: "01",
+  },
+  {
+    id: 2,
+    title: "BANKROLLS & POLES",
+    duration: 0,
+    track_number: "02",
+  },
+  {
+    id: 3,
+    title: "TRAP JUMPIN",
+    duration: 0,
+    track_number: "03",
+  },
+  {
+    id: 4,
+    title: "FULL EFFECT",
+    duration: 0,
+    track_number: "04",
+  },
+  {
+    id: 5,
+    title: "INTL' PLAYA",
+    duration: 0,
+    track_number: "05",
+  },
+  {
+    id: 6,
+    title: "ROADRUNNER",
+    duration: 0,
+    track_number: "06",
+  },
+  {
+    id: 7,
+    title: "R.B.I.T.K",
+    duration: 0,
+    track_number: "07",
+  },
+  {
+    id: 8,
+    title: "GOT IT BACK",
+    duration: 0,
+    track_number: "08",
+  },
+  {
+    id: 9,
+    title: "WHY LIE",
+    duration: 0,
+    track_number: "09",
+  },
+  {
+    id: 10,
+    title: "WHY THEY MAD",
+    duration: 0,
+    track_number: "10",
+  },
+  {
+    id: 11,
+    title: "CRAZY WORLD",
+    duration: 0,
+    track_number: "11",
+  },
+  {
+    id: 12,
+    title: "HOW I FEEL",
+    duration: 0,
+    track_number: "12",
+  },
+];
+
+export const ALBUM_INFO = {
+  title: "401K",
+  artist: "YolaJo",
+  artworkUrl: "/artwork/401k.jpg",
+  releaseYear: 2026,
+  totalTracks: 12,
+};
