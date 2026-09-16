95d5d157c7331564cc80078183ac4d939db3575a
isaahaqq-star <isaahaqq@gmail.com>
2026-09-16 12:06:36 -0400

    Update page.tsx

--- player/app/page.tsx
+++ player/app/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Player } from '@/components/Player'
+import Player from '@/components/Player'
 import '@/styles/globals.css'
 
 export const metadata: Metadata = {
